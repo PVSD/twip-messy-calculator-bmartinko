b7f309bb82148fc0b9b67ba75c07e3bd2b0bb119
bmartinko <bennett.martinko@gmail.com>
2018-09-11 11:23:59 -0400

    Attempt #1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,94 @@
 package com.company;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner kbReader = new Scanner(System.in);
+        System.out.println("Which operation would you like to perform?");
+        String operation = kbReader.nextLine();
+        System.out.println("1st Number:");
+        int firstNumber = kbReader.nextInt();
+        System.out.println("2nd Number:");
+        int secondNumber = kbReader.nextInt();
+        boolean correctGuess = false;
+        String answer = "log";
+
+        if (operation == "addition") {
+            double equationA = Math.log(firstNumber + secondNumber);
+            System.out.println("The sum of " + firstNumber + " and " + secondNumber + "= " + equationA);
+            while (!correctGuess) {
+                System.out.println("Are you satisfied?");
+                String response = kbReader.nextLine();
+                if (response == "yes") {
+                    System.out.println("You have been misinformed");
+                } else if (response == "no") {
+                    System.out.println("What happened to the equation?");
+                    String responseB = kbReader.nextLine();
+                    if (responseB == answer) {
+                        System.out.println("Good Job!");
+                    } else {
+                        System.out.println("Nope! Keep on trying!");
+                    }
+                }
+            }
+        } else if (operation == "subtraction") {
+            double equationB = Math.log(firstNumber - secondNumber);
+            System.out.println("The subtraction of " + firstNumber + " and " + secondNumber + "= " + equationB);
+            while (!correctGuess) {
+                System.out.println("Are you satisfied?");
+                String response = kbReader.nextLine();
+                if (response == "yes") {
+                    System.out.println("You have been misinformed");
+                } else if (response == "no") {
+                    System.out.println("What happened to the equation?");
+                    String responseB = kbReader.nextLine();
+                    if (responseB == answer) {
+                        System.out.println("Good Job!");
+                    } else {
+                        System.out.println("Nope! Keep on trying!");
+                    }
+                }
+            }else if (operation == "multiplication") {
+                double equationC = Math.log(firstNumber * secondNumber);
+                System.out.println("The multiplication of " + firstNumber + " and " + secondNumber + "= " + equationC);
+
+                while (!correctGuess) {
+                    System.out.println("Are you satisfied?");
+                    String response = kbReader.nextLine();
+                    if (response == "yes") {
+                        System.out.println("You have been misinformed");
+                    } else if (response == "no") {
+                        System.out.println("What happened to the equation?");
+                        String responseB = kbReader.nextLine();
+                        if (responseB == answer) {
+                            System.out.println("Good Job!");
+                        } else {
+                            System.out.println("Nope! Keep on trying!");
+                        }
+                    }
+                }else if (operation == "division") {
+                    double equationD = Math.log(firstNumber / secondNumber);
+                    System.out.println("The division of " + firstNumber + " and " + secondNumber + "= " + equationD);
+                    while (!correctGuess) {
+                        System.out.println("Are you satisfied?");
+                        String response = kbReader.nextLine();
+                        if (response == "yes") {
+                            System.out.println("You have been misinformed");
+                        } else if (response == "no") {
+                            System.out.println("What happened to the equation?");
+                            String responseB = kbReader.nextLine();
+                            if (responseB == answer) {
+                                System.out.println("Good Job!");
+                            } else {
+                                System.out.println("Nope! Keep on trying!");
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
     }
 }
