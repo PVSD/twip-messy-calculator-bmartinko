9c52b7ec5f34e517b995241f82b30f892c3bde0d
bmartinko <bennett.martinko@gmail.com>
2018-09-14 00:05:03 -0400

    Calculator finally done!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,73 +15,78 @@ public class Main {
         boolean correctGuess = false;
         String answer = "log";
 
-        if (operation == "addition") {
+        if (operation.equalsIgnoreCase("addition")){
             double equationA = Math.log(firstNumber + secondNumber);
-            System.out.println("The sum of " + firstNumber + " and " + secondNumber + "= " + equationA);
+            System.out.println("The sum of " + firstNumber + " and " + secondNumber + " = " + equationA);
+            System.out.println("Are you satisfied?");
             while (!correctGuess) {
-                System.out.println("Are you satisfied?");
                 String response = kbReader.nextLine();
-                if (response == "yes") {
+                if (response.equalsIgnoreCase("yes")){
                     System.out.println("You have been misinformed");
-                } else if (response == "no") {
+                } else if (response.equalsIgnoreCase("no")) {
                     System.out.println("What happened to the equation?");
                     String responseB = kbReader.nextLine();
-                    if (responseB == answer) {
+                    if (responseB.equalsIgnoreCase(answer)){
                         System.out.println("Good Job!");
+                        correctGuess = true;
                     } else {
                         System.out.println("Nope! Keep on trying!");
                     }
                 }
             }
-        } else if (operation == "subtraction") {
+        } else if (operation.equalsIgnoreCase("subtraction")) {
             double equationB = Math.log(firstNumber - secondNumber);
-            System.out.println("The subtraction of " + firstNumber + " and " + secondNumber + "= " + equationB);
+            System.out.println("The subtraction of " + firstNumber + " and " + secondNumber + " = " + equationB);
+            System.out.println("Are you satisfied?");
             while (!correctGuess) {
-                System.out.println("Are you satisfied?");
                 String response = kbReader.nextLine();
-                if (response == "yes") {
+                if (response.equalsIgnoreCase("yes")) {
                     System.out.println("You have been misinformed");
-                } else if (response == "no") {
+                } else if (response.equalsIgnoreCase("no")) {
                     System.out.println("What happened to the equation?");
                     String responseB = kbReader.nextLine();
-                    if (responseB == answer) {
+                    if (responseB.equalsIgnoreCase(answer)) {
                         System.out.println("Good Job!");
+                        correctGuess = true;
                     } else {
                         System.out.println("Nope! Keep on trying!");
                     }
                 }
-            } if (operation == "multiplication") {
+            }
+        }else if (operation.equalsIgnoreCase("multiplication")) {
                 double equationC = Math.log(firstNumber * secondNumber);
-                System.out.println("The multiplication of " + firstNumber + " and " + secondNumber + "= " + equationC);
-
+                System.out.println("The multiplication of " + firstNumber + " and " + secondNumber + " = " + equationC);
+                System.out.println("Are you satisfied?");
                 while (!correctGuess) {
-                    System.out.println("Are you satisfied?");
                     String response = kbReader.nextLine();
-                    if (response == "yes") {
+                    if (response.equalsIgnoreCase("yes")) {
                         System.out.println("You have been misinformed");
-                    } else if (response == "no") {
+                    } else if (response.equalsIgnoreCase("no")) {
                         System.out.println("What happened to the equation?");
                         String responseB = kbReader.nextLine();
-                        if (responseB == answer) {
+                        if (responseB.equalsIgnoreCase(answer)) {
                             System.out.println("Good Job!");
+                            correctGuess = true;
                         } else {
                             System.out.println("Nope! Keep on trying!");
                         }
                     }
-                } if (operation == "division") {
+                }
+            }else if (operation.equalsIgnoreCase("division")){
                     double equationD = Math.log(firstNumber / secondNumber);
-                    System.out.println("The division of " + firstNumber + " and " + secondNumber + "= " + equationD);
+                    System.out.println("The division of " + firstNumber + " and " + secondNumber + " = " + equationD);
+                    System.out.println("Are you satisfied?");
                     while (!correctGuess) {
-                        System.out.println("Are you satisfied?");
                         String response = kbReader.nextLine();
-                        if (response == "yes") {
+                        if (response.equalsIgnoreCase("yes")){
                             System.out.println("You have been misinformed");
-                        } else if (response == "no") {
+                        } else if (response.equalsIgnoreCase("no")){
                             System.out.println("What happened to the equation?");
                             String responseB = kbReader.nextLine();
-                            if (responseB == answer) {
+                            if (responseB.equalsIgnoreCase(answer)){
                                 System.out.println("Good Job!");
-                            } else {
+                                correctGuess = true;
+                            }else {
                                 System.out.println("Nope! Keep on trying!");
                             }
                         }
@@ -90,5 +95,3 @@ public class Main {
                 }
             }
         }
-    }
-}
